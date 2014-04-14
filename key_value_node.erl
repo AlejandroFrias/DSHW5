6f8812f5a97b15b21fd2190c2221aa02da79468e
David Scott <dscott@hmc.edu>
2014-04-13 19:08:23 -0700

    halfway through fault tolerance

--- key_value_node.erl
+++ key_value_node.erl
@@ -3,69 +3,77 @@
 
 -export([main/1]).
 
-%Filter for handlers only
+%%Filter for handlers only
 handlerFilter(Names) ->
-	[X || X <- Names, utils:isHandler(X)].
+    [X || X <- Names, utils:isHandler(X)].
 
-%progress!
+%%progress!
 maxConsecutiveDifference([_], Max, First, Second) -> {Max, First, Second}; 
 maxConsecutiveDifference([F | Nums], Max, First, Second) ->
-	S = hd(Nums),
-	if (S - F) > Max -> maxConsecutiveDifference(Nums, S-F, F, S);
-	true -> maxConsecutiveDifference(Nums, Max, First, Second)
-end.
+    S = hd(Nums),
+    if (S - F) > Max -> maxConsecutiveDifference(Nums, S-F, F, S);
+       true -> maxConsecutiveDifference(Nums, Max, First, Second)
+    end.
 
-%Find the widest gap in node locations, then fill it
-%Take in a list of handler names
-%Return the midpoint as a name, and the next name since we need that too
+%%Find the widest gap in node locations, then fill it
+%%Take in a list of handler names
+%%Return the midpoint as a name, and the next name since we need that too
 findWidestHandlerGap(Names, M) ->
-	TwoM = 1 bsl M,
-	Nums = [utils:getID(N) || N <- Names],
-	SortedNums = lists:sort(Nums),
-	NumsExtended = SortedNums ++ [hd(SortedNums) + TwoM],
-	{_, First, Second} = maxConsecutiveDifference(NumsExtended, 0, 0, 0),
-	%We mod by the range to ensure that we get values within the range, so not like past 1024. Good comments guys
-	{First rem TwoM, (First + ((Second - First) div 2)) rem TwoM, Second rem TwoM}.
+    TwoM = 1 bsl M,
+    Nums = [utils:getID(N) || N <- Names],
+    SortedNums = lists:sort(Nums),
+    NumsExtended = SortedNums ++ [hd(SortedNums) + TwoM],
+    {_, First, Second} = maxConsecutiveDifference(NumsExtended, 0, 0, 0),
+    %%We mod by the range to ensure that we get values within the range, so not like past 1024. Good comments guys
+    {First rem TwoM, (First + ((Second - First) div 2)) rem TwoM, Second rem TwoM}.
 
 
 
 main([M, Name]) -> 
-	Minty = list_to_integer(M),
-	%Erlang networking boilerplate
-	_ = os:cmd("epmd -daemon"),
+    Minty = list_to_integer(M),
+    %%Erlang networking boilerplate
+    _ = os:cmd("epmd -daemon"),
     net_kernel:start([list_to_atom(Name), shortnames]),
 
-	%Start the SH
-	utils:log("Starting storage handler with ID ~w", [0]),
-	utils:log("Registering with name ~w", [utils:hname(0)]),
-	
-	gen_server:start({global, utils:hname(0)}, handler, {Minty, 0}, []),
+    %%Start the SH
+    utils:log("Starting storage handler with ID ~w", [0]),
+    utils:log("Registering with name ~w", [utils:hname(0)]),
 
- 	timer:sleep(10000);
+    handler:start({Minty});
 
 main([M, Name, Other]) -> 
-	Minty = list_to_integer(M),
+    Minty = list_to_integer(M),
 
-  % Erlang networking boilerplate 
-  utils:log("Starting node with name ~w", [Name]),
-  _ = os:cmd("epmd -daemon"),
-  net_kernel:start([list_to_atom(Name), shortnames]),
+    %% Erlang networking boilerplate 
+    utils:log("Starting node with name ~w", [Name]),
+    _ = os:cmd("epmd -daemon"),
+    net_kernel:start([list_to_atom(Name), shortnames]),
 
-  %Connect to the specified node
-  ConnectResult = net_kernel:connect_node(list_to_atom(Other)),
-  utils:log("Connecting to ~w, result is: ~w", [Other, ConnectResult]),
+    OtherStr = list_to_atom(Other),
+    %%Connect to the specified node
+    ConnectResult = net_kernel:connect_node(OtherStr),
+    utils:log("Connecting to ~p, result is: ~p", [OtherStr, ConnectResult]),
 
-  %Sleep to let the global names sync
-  timer:sleep(1000),
-  
-  %Compute right place to start
-  Names = global:registered_names(),
-  %utils:log("Registered names: ~w", [Names]),
-  HandlerNames = handlerFilter(Names),
-  utils:log("Finding where we should go among handlers ~w", [HandlerNames]),
-  {PrevHandlerID, NewHandlerID, NextHandlerID} = findWidestHandlerGap(HandlerNames, Minty),
+    %%Sleep to let the global names sync
+    %%timer:sleep(1000),
+    global:sync(),
 
-	%Start the SH
-	utils:log("Starting storage handler with ID ~w", [NewHandlerID]),
-	gen_server:start({global, utils:hname(NewHandlerID)}, handler, {Minty, PrevHandlerID, NewHandlerID, NextHandlerID}, []).
+    %%Compute right place to start
+    Names = global:registered_names(),
+    utils:log("Registered names: ~w", [Names]),
+    HandlerNames = handlerFilter(Names),
+    utils:log("Finding where we should go among handlers ~w", [HandlerNames]),
+    {PrevHandlerID, NewHandlerID, NextHandlerID} = findWidestHandlerGap(HandlerNames, Minty),
+
+    %%Start the SH
+    utils:log("Starting storage handler with ID ~w", [NewHandlerID]),
+    utils:log("Registering with name ~w", [utils:hname(NewHandlerID)]),
+
+    handler:start( {Minty, PrevHandlerID, NewHandlerID, NextHandlerID} ),
+    
+    %% gen_server:start({global, utils:hname(NewHandlerID)}, handler, {Minty, PrevHandlerID, NewHandlerID, NextHandlerID}, []),
+    %% timer:sleep(1000),
+
+    %% Sanity checking for debugging.
+    utils:log("Registered names: ~p", [global:registered_names()]).
 
