1424aed6b92a6661cc2f40cc7220176411eb996c
Alejandro Frias <afrias@g.hmc.edu>
2014-04-15 21:36:19 -0700

    Sorry guys

--- utils.erl
+++ utils.erl
@@ -110,7 +110,7 @@ getID(Name) ->
 
 %% returns the distance between two numbers, A and B, when there are 2^M numbers 
 %% and it wraps around. Finds the distance moving in the positive direction only.
-modDist(M, A, B) ->
+modDist(A, B, M) ->
   Dist = (B - A) rem pow2(M), 
   (Dist + pow2(M)) rem pow2(M).
 
